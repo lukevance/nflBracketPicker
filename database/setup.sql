5a433a51da2efee28adff07f8f54cc62afb44a1d
Luke Vance <lukevance90@gmail.com>
2016-01-05 14:47:15 -0700

    initial commit

---
+++ database/setup.sql
@@ -0,0 +1,16 @@
+DROP TABLE users;
+CREATE TABLE users (
+    id          SERIAL PRIMARY KEY,
+    firstname   VARCHAR(50),
+    lastname    VARCHAR(50),
+    email       VARCHAR(50),
+    username    varchar(50),
+    password    varchar(256),
+    score       INTEGER
+);
+
+CREATE TABLE week1picks (
+    id          SERIAL,
+    cin_pit     VARCHAR,
+    
+);
