1f0b6bd6fbe1ba71c36ac5fc2c3bc13932bf5273
Luke Vance <lukevance90@gmail.com>
2016-01-08 15:08:03 -0700

    user can now see braket generated from database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var knex = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+});
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('user');
+router.get('/:id', function(req, res, next) {
+  // console.log(req.params.id);
+  knex('picks').where({
+    user_id : req.params.id
+  }).then(function(pickData) {
+    console.log(pickData);
+    var afc_3 = '3. Cincinatti';
+    var afc_4 = '4. Houston';
+    var afc_5 = '5. Kansas City';
+    var afc_6 = '6. Pittsburgh';
+    var nfc_3 = '3. Minnesota';
+    var nfc_4 = '4. Washington';
+    var nfc_5 = '5. Green Bay';
+    var nfc_6 = '6. Seattle';
+
+
+    var plusWildCard = pickData[0];
+    // afc wildcard logic
+    if (plusWildCard.afc_g1[0] === 'c') {
+      plusWildCard.afcw1 = afc_4;
+      plusWildCard.afcw2 = afc_5;
+      plusWildCard.afcw3 = afc_3;
+      plusWildCard.afcw4 = afc_6;
+    } else if (plusWildCard.afc_g1[0] === 'p') {
+      plusWildCard.afcw1 = afc_3;
+      plusWildCard.afcw2 = afc_6;
+      plusWildCard.afcw3 = afc_4;
+      plusWildCard.afcw4 = afc_5;
+    }
+    // nfc wildcard logic
+    if (plusWildCard.nfc_g1[0] === 'm') {
+      plusWildCard.nfcw1 = nfc_4;
+      plusWildCard.nfcw2 = nfc_5;
+      plusWildCard.nfcw3 = nfc_3;
+      plusWildCard.nfcw4 = nfc_6;
+    } else if (plusWildCard.nfc_g1[0] === 's') {
+      plusWildCard.nfcw1 = nfc_3;
+      plusWildCard.nfcw2 = nfc_6;
+      plusWildCard.nfcw3 = nfc_4;
+      plusWildCard.nfcw4 = nfc_5;
+    }
+
+    console.log(plusWildCard);
+    res.render('user', {data:plusWildCard});
+  });
 });
 
 router.get('/bracket', function(req, res, next) {
