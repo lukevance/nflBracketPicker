a11bf313b9b1b65c27112cea76c3886a6494882d
LKHohn <lkhohn@gmail.com>
2016-01-08 15:00:03 -0700

    signin errors handled

--- routes/signin.js
+++ routes/signin.js
@@ -19,52 +19,52 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 
   knex('users').where({
-    username : req.body.username
-    // password: req.body.password
+    username: req.body.username
+      // password: req.body.password
   }).then(function(user) {
     console.log(req.body.password);
     console.log('this is the password from the db: ' + user[0].password);
 
-    function redirect (destination) {
+    function redirect(destination) {
       res.redirect(destination);
     }
 
-    function next (user, status) {
+    function next(user, status) {
       // console.log(status);
       console.log(user);
       if (status === true) {
         //set cookie
         req.session.user = {
-          username : user.username,
+          username: user.username,
         };
         console.log(req.session.user);
-        redirect('/');
+        res.redirect('/');
 
       } else {
-        res.send('incorrect username or password');
+        res.render('signin', {
+          message: "Incorrect username or password"
+        });
       }
     }
-
     if (user[0]) {
       // console.log(user);
       // username/password is correct
       comparePassword(req.body.password, user[0], next);
-
     } else {
-      console.log('err');
+      res.send('incorrect username or password');
     }
-
+  }).catch(function(err) {
+    // if whole thing doesn't work
+    res.render('signin', {
+    message: "Incorrect username or password"
+    });
   });
-  // .catch(function(err) {
-  //   if whole thing doesn't work
-  //   console.log(err);
-  // });
 });
 
-function comparePassword (password, user, callback) {
-  bcrypt.compare(password, user.password, function(err, res){
+function comparePassword(password, user, callback) {
+  bcrypt.compare(password, user.password, function(err, res) {
     // console.log(err);
-      callback(user, res);
+    callback(user, res);
   });
 }
 
